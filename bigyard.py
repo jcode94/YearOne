80727a86119eb44570b3ed6635a82b8cafc2dbc4
Justice Smith <59818423+jcode94@users.noreply.github.com>
2020-02-07 14:50:33 -0500

    Problem 1
    incorporated new-style string formatting.

--- bigyard.py
+++ bigyard.py
@@ -5,20 +5,17 @@
 
 import math
 
-totalFencing = 0
+totalFencing = int(input("How much fencing, in feet?"))
+print("Your available dimensions are:")
+for x in range(1, totalFencing):
+    y = (totalFencing/2)-x
+    area = x * y
+    if y < x:
+        break
+    print("{} by {} with area {}" .format(x, y, area))
 
-# Greet
-print("Welcome! We're going to calculate the fencing dimensions available to you.\n")
-# User initial input; what we have to work with
-totalFencing = int(input("How much fencing do you have, in feet?")
 
-while True:
-    for lowdim in range(4, math.sqrt(totalFencing)):
-        if totalFencing%lowdim == 0:
-            highdim = totalFencing/lowdim
-            print("%d by %d", lowdim highdim)
 
-print("The dimensions available to you are:\n %d by %")
 
 # planning:
 
@@ -29,4 +26,3 @@ print("The dimensions available to you are:\n %d by %")
 # Also, the last line may have X = Y.
 
 # need X factors with % = 0
-
