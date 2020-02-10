815dbb991b53cf8ab8946c2f3b3b4733f6d57856
Justice Smith <59818423+jcode94@users.noreply.github.com>
2020-02-09 20:44:03 -0500

    updates for final submission
    bigyard - added prime set for unfactorable dimensions, made it an actual loop
    
    tennis - final sub with clear whitespacing and planning

--- bigyard.py
+++ bigyard.py
@@ -3,20 +3,6 @@
 # COP2930-Arup Guha
 # Individual Prog Ass't 2
 
-import math
-
-totalFencing = int(input("How much fencing, in feet?"))
-print("Your available dimensions are:")
-for x in range(1, totalFencing):
-    y = (totalFencing/2)-x
-    area = x * y
-    if y < x:
-        break
-    print("{} by {} with area {}" .format(x, y, area))
-
-
-
-
 # planning:
 
 # output needs: Write out a single line of the form
@@ -25,4 +11,34 @@ for x in range(1, totalFencing):
 # Order the rectangles by increasing order of X.
 # Also, the last line may have X = Y.
 
-# need X factors with % = 0
+
+# The unfactorables under 100(the predetermined input set)...
+primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+
+# User input
+print("How much fencing, in feet?")
+
+# Check for unfactorable primes.
+while True:
+    # Get totalFencing
+    totalFencing = int(input(""))
+    # Check to see if input is in prime list
+    if totalFencing in primes:
+        print("Your number cannot be a prime number. Please enter a composite integer.")
+        # Cont. allows for new input
+        continue
+    else:
+        print("Your available dimensions are:")
+        for x in range(1, totalFencing, 1):
+            y = (totalFencing/2)-x
+            area = x * y
+            if y < x:
+                break
+            print("{} by {} with area {}" .format(x, int(y), int(area)))
+
+
+
+
+
+
+
--- tennis.py
+++ tennis.py
@@ -1,42 +1,54 @@
 # Justice Smith
-# 02.07.20
-# COP2930
+# 02.05.2020
+# COP2930-Arup Guha
+# Individual Prog Ass't 2
 
 # planning
 # match is first to 2 sets.
 # set is first to 6 with 2 game lead(e.g. first to 6-4 or 7-6 tiebreaker
 
+# Create variables
 serenaWins = 0
 rogerWins = 0
 serenaSets = 0
 rogerSets = 0
 
+# Single iteration instructions, outside looping
 print("Please enter the game winners, in sequence.\n")
 
+
 while True:
+    # First check to break out of topmost level, checks after every input
     if serenaSets == 2 or rogerSets == 2:
         if rogerSets > serenaSets:
+            # Juicy new-style str formatting
             print("ROGER won the game 2 sets to {} sets.".format(serenaSets))
             break
         else:
             print("SERENA won the game 2 sets to {}.".format(rogerSets))
         break
 
+    # Open, repeating input
     gameWinner = input()
 
+    # 1 of 2 topmost logic forks
     if gameWinner == "SERENA":
         serenaWins += 1
 
+        # Set incrementer for set win condition one
         if serenaWins == 6 and serenaWins >= (rogerWins + 2):
+            # Have to reset win counter
             serenaWins = 0
             rogerWins = 0
             serenaSets += 1
 
+        # Set incrementer for set win condition two
         if serenaWins == 7 and rogerWins == 6:
             serenaWins = 0
             rogerWins = 0
             serenaSets += 1
 
+    # 2 of 2 topmost level forks
     if gameWinner == "ROGER":
         rogerWins += 1
 
@@ -50,6 +62,7 @@ while True:
             serenaWins = 0
             rogerSets += 1
 
+    #Basic input response for anything not two acceptable inputs.
     else:
         print("Error: Unrecognized Input. Please enter either ROGER or SERENA.")
         continue
